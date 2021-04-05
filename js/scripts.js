34cb0333edafac605e03236a0ab6b92c0064115d
cass1618 <cass16180@gmail.com>
2021-04-05 10:56:12 -0700

    Add contact object
    Co-authored-by: Grafton Borge <graftonmborge@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
 // Business Logic for AddressBook ---------
 function AddressBook() {
   this.contacts = {};
@@ -29,31 +40,31 @@ AddressBook.prototype.deleteContact = function(id) {
   return true;
 }
 
-// Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.phoneNumber = phoneNumber;
-}
+// // Business Logic for Contacts ---------
+// function Contact(firstName, lastName, phoneNumber) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.phoneNumber = phoneNumber;
+// }
 
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
-}
+// Contact.prototype.fullName = function() {
+//   return this.firstName + " " + this.lastName;
+// }
 
-// User Interface Logic ---------
-let addressBook = new AddressBook();
+// // User Interface Logic ---------
+// let addressBook = new AddressBook();
 
-$(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
-    event.preventDefault();
-    const inputtedFirstName = $("input#new-first-name").val();
-    const inputtedLastName = $("input#new-last-name").val();
-    const inputtedPhoneNumber = $("input#new-phone-number").val();
-    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
-    addressBook.addContact(newContact);
-    console.log(addressBook.contacts);
-  });
-});
+// $(document).ready(function() {
+//   $("form#new-contact").submit(function(event) {
+//     event.preventDefault();
+//     const inputtedFirstName = $("input#new-first-name").val();
+//     const inputtedLastName = $("input#new-last-name").val();
+//     const inputtedPhoneNumber = $("input#new-phone-number").val();
+//     let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+//     addressBook.addContact(newContact);
+//     console.log(addressBook.contacts);
+//   });
+// });
 
 // function Contact(name, address, age , phone) {
 //   this.name = name;
