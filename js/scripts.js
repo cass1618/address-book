600da1da5a3454a6fc1ad2d95a7367949e8cc3dc
cass1618 <cass16180@gmail.com>
2021-04-05 09:37:37 -0700

    Add initial html and js files
    Co-authored-by: Grafton Borge <graftonmborge@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function Dog(name, colors, age , owner) {
+  this.name = name;
+  this.colors = colors;
+  this.age = age;
+  this.owner = owner;
+}
+
+let falcor = new Dog("Falcor", ["black"], 4);
+let nola = new Dog("Nola", ["white", "black"], 6);
+let patsy = new Dog("Patsy", ["brown"], 7);
+
+
